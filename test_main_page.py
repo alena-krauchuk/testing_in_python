975bbe18fa77d58840f11fa105cef16140c8e35d
Elena <peregovorun@gmail.com>
2023-07-19 20:37:18 +0300

    update test for checking of transfer to login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,10 +8,13 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                     # открываем страницу
     page.go_to_login_page()         # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                         # открываем страницу
     page.should_be_login_link()         # выполняем метод страницы — находим ссылку на страницу логина
